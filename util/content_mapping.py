494024f2a44182caf929a4b685be0d50cd47c0c6
Michael Crinite <mikecrinite@gmail.com>
2017-10-05 22:17:22 -0400

    Bug fixes related to using the wrong list, added correct :b: emoji

--- util/content_mapping.py
+++ util/content_mapping.py
@@ -6,6 +6,14 @@ for. each target word maps to a list containing
     - the reply (should the bot say anything in response?)
     - a reaction (should the bot react to the previous message?)
     - True/False indicating whether the bot should stop looking for keywords
+
+If you want to add something, make sure you add it to the correct mapping:
+    - listen_mappings: if the key shows up in any message the bot can see, the bot will
+      add the reaction and send the response
+    - mention_mappings: if the key shows up in a message that mentions the bot, it will
+      add the reaction and send the response
+
+We can probably consolidate those two functions at one point....
 """
 listen_mappings = {
     'lol': ['', '🍭', False],
@@ -14,9 +22,10 @@ listen_mappings = {
 }
 
 mention_mappings = {
+    'repo': ['Check me out: https://github.com/mikecrinite/wednesday-bot', '', True],
     'fuck you': ['I\'m sorry you feel that way, my guy', '😢', True],
     'diabetes': ['Thankfully, frogs don\'t get diabetes.', '', False],
-    'bonzi': ['#fuckbonzi', ':B:', False]
+    'bonzi': ['#fuckbonzi', '🅱', False]
 }
 
 
@@ -27,10 +36,10 @@ def listen_to(message):
     """
     l = []
     for key in listen_mappings:
-        if key in message.content.lower:
+        if key in message:
             v = listen_mappings[key]
             l.append(v)
-            if stop_processing(l):
+            if stop_processing(v):
                 break
     return l
 
@@ -42,8 +51,11 @@ def mentioned_in(message):
     """
     m = []
     for key in mention_mappings:
-        if key in message.content.lower:
-            m.append(mention_mappings[key])
+        if key in message:
+            v = mention_mappings[key]
+            m.append(v)
+            if stop_processing(v):
+                break
     return m
 
 
