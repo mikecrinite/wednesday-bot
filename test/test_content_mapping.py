bbd8f657ffd5748bc1e0a773eb1d63465c926c3e
Michael Crinite <mikecrinite@gmail.com>
2017-10-10 20:32:11 -0400

    test content_mapping methods

--- test/test_content_mapping.py
+++ test/test_content_mapping.py
@@ -8,10 +8,22 @@ class TestContentMappingMethods(unittest.TestCase):
         return
 
     def test_listen_to(self):
-        return
+        response = content_mapping.listen_to("lol, shit")
+        self.assertTrue(['', '🍭', False] in response)
+        self.assertTrue(['', '💩', False] in response)
+        self.assertEqual(content_mapping.listen_to("nothing here"), [])
 
     def test_mentioned_in(self):
-        return
+        response0 = content_mapping.mentioned_in("diabetes, bonzi")
+        self.assertTrue(['Thankfully, frogs don\'t get diabetes.', '', False] in response0)
+        self.assertTrue(['#fuckbonzi', '🅱', False] in response0)
+
+        response1 = content_mapping.mentioned_in("fuck you, diabetes!")
+        self.assertTrue(['I\'m sorry you feel that way, my guy', '😢', True] in response1)
+        self.assertEqual(len(response1), 1)
+
+        response2 = content_mapping.mentioned_in("nothing here, fellas")
+        self.assertEqual(response2, [])
 
     def tearDown(self):
         return
