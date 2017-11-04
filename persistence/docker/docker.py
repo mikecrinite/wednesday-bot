7ce193072ec076440b484edcce5e212e3f6a24aa
Michael Crinite <mikecrinite@gmail.com>
2017-11-03 23:00:20 -0400

    I forget what I did

--- persistence/docker/docker.py
+++ persistence/docker/docker.py
@@ -7,7 +7,7 @@ docker_logger.setLevel(logging.INFO)
 
 # Setup Docker, and start mysql container
 client = docker.from_env()
-client.containers.run("mysql/mysql", detach=True)
+client.containers.run("mysql/mysql")  # , detach=True)
 
 # Point a reference at the mysql container
 all_containers = client.containers.list()
@@ -25,6 +25,7 @@ else:
 
 # Do things
 mysql.exec_run('echo lmaoooooooooo')
+
 
 def stream_logs():
     """
